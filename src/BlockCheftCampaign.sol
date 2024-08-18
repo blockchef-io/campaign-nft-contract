19dfd935f8c11ac29de5cf12e42abc30cb173c05
FarajiOranj <farajioranj@gmail.com>
2024-08-18 17:53:33 +0330

    base contract coded

--- src/BlockCheftCampaign.sol
+++ src/BlockCheftCampaign.sol
@@ -2,7 +2,12 @@
 pragma solidity 0.8.21;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract BlockCheftCNFT is ERC721 {
+    using Strings for *;
 
+    constructor() ERC721("BlockChef Campaign NFT", "BC-CNFT") {}
+
+    function tokenURI(uint256 id) public view override returns (string memory) {}
 }

0b58a8e4e8fc0ee909e926bfc2eb9f2b117b3aa8
FarajiOranj <farajioranj@gmail.com>
2024-08-18 17:45:04 +0330

    solmate ERC721 import path added

--- src/BlockCheftCampaign.sol
+++ src/BlockCheftCampaign.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-contract BlockCheftCNFT {
-    
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
+contract BlockCheftCNFT is ERC721 {
+
 }

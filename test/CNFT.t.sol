11322c649c41a0794c85eeabda66344312da7aef
FarajiOranj <farajioranj@gmail.com>
2024-08-19 03:43:56 +0330

    improved

--- test/CNFT.t.sol
+++ test/CNFT.t.sol
@@ -8,25 +8,24 @@ import {BlockChefCampaignNFT} from "../src/BlockChefCampaignNFT.sol";
 
 contract BlockChefCampaignNFTTest is Test {
     BlockChefCampaignNFT private nft;
-    address private user1 = address(0x4567);
-    address private user2 = address(0x89AB);
+    string private IPFS_URL = "https://ipfs.io/ipfs/";
+    string private TEST_CID = "QmTestCID";
+    address private immutable OWNER = vm.addr(OWNER_PK);
+    address private immutable USER1 = address(0x4567);
+    address private immutable USER2 = address(0x89AB);
+    uint256 private constant OWNER_PK = 0x0123;
 
-    function owner() private returns (address addr, uint256 pk) {
-        (addr, pk) = makeAddrAndKey("owner");
-    }
-
-    function setUp() public {
-        nft = new BlockChefCampaignNFT("https://ipfs.io/ipfs/");
-        address _owner;
-        (_owner, ) = owner();
-        nft.transferOwnership(_owner);
+    // Config
+    function setUp() external {
+        vm.prank(OWNER);
+        nft = new BlockChefCampaignNFT(IPFS_URL);
     }
 
     // Test Constructor
     function test_ConstructorInitialState() external view {
         assertEq(nft.MINTABLE(), true);
         assertEq(nft.TRANSFERABLE(), false);
-        assertEq(nft.IPFS_URL(), "https://ipfs.io/ipfs/");
+        assertEq(nft.IPFS_URL(), IPFS_URL);
         assertEq(nft.totalSupply(), 0);
     }
 
@@ -37,37 +36,31 @@ contract BlockChefCampaignNFTTest is Test {
 
     // Test Only Owner
     function test_OnlyOwnerCanLockMinting() external {
-        vm.prank(user1);
+        vm.prank(USER1);
         vm.expectRevert(
             abi.encodeWithSelector(
                 Ownable.OwnableUnauthorizedAccount.selector,
-                user1
+                USER1
             )
         );
         nft.oneTimeMintLocker();
 
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
+        vm.prank(OWNER);
         nft.oneTimeMintLocker();
         assertEq(nft.MINTABLE(), false);
     }
 
     function test_OnlyOwnerCanUnlockTransfers() external {
-        vm.prank(user1);
+        vm.prank(USER1);
         vm.expectRevert(
             abi.encodeWithSelector(
                 Ownable.OwnableUnauthorizedAccount.selector,
-                user1
+                USER1
             )
         );
         nft.oneTimeTransferUnlocker();
 
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
+        vm.prank(OWNER);
         nft.oneTimeTransferUnlocker();
         assertEq(nft.TRANSFERABLE(), true);
     }
@@ -75,25 +68,17 @@ contract BlockChefCampaignNFTTest is Test {
     function test_OwnerCanWithdrawFunds() external {
         vm.deal(address(nft), 1 ether);
 
-        address _owner;
-        (_owner, ) = owner();
+        uint256 initialOwnerBalance = OWNER.balance;
 
-        uint256 initialOwnerBalance = _owner.balance;
-
-        vm.prank(_owner);
+        vm.prank(OWNER);
         nft.withrawETH();
-
-        assertEq(_owner.balance, initialOwnerBalance + 1 ether);
+        assertEq(OWNER.balance, initialOwnerBalance + 1 ether);
     }
 
     // Test Transfer
     function test_TransferFailsWhenLocked() external {
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
         nft.mint(
-            user1,
+            USER1,
             BlockChefCampaignNFT.cNFT(
                 BlockChefCampaignNFT.cNFTtype.Bronze,
                 "",
@@ -101,18 +86,14 @@ contract BlockChefCampaignNFTTest is Test {
             )
         );
 
-        vm.prank(user1);
+        vm.prank(USER1);
         vm.expectRevert("TRANSFER_LOCKED");
-        nft.transferFrom(user1, user2, 0);
+        nft.transferFrom(USER1, USER2, 0);
     }
 
     function test_TransferSucceedsWhenUnlocked() external {
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
         nft.mint(
-            user1,
+            USER1,
             BlockChefCampaignNFT.cNFT(
                 BlockChefCampaignNFT.cNFTtype.Bronze,
                 "",
@@ -120,122 +101,121 @@ contract BlockChefCampaignNFTTest is Test {
             )
         );
 
-        vm.prank(_owner);
+        vm.prank(OWNER);
         nft.oneTimeTransferUnlocker();
 
-        vm.prank(user1);
-        nft.transferFrom(user1, user2, 0);
-
-        assertEq(nft.ownerOf(0), user2);
+        vm.prank(USER1);
+        nft.transferFrom(USER1, USER2, 0);
+        assertEq(nft.ownerOf(0), USER2);
     }
 
     // Test Minting
     function test_MintingWithValidSignature() external {
-        uint256 ownerPK;
-        (, ownerPK) = owner();
-        string memory cid = "QmTestCID";
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPK, keccak256(bytes(cid)));
-        // This should be a valid signature
-        bytes memory signature = abi.encodePacked(r, s, v); // note the order here is different from line above.
-
-        BlockChefCampaignNFT.cNFT memory newCNFT = BlockChefCampaignNFT.cNFT(
-            BlockChefCampaignNFT.cNFTtype.Gold,
-            cid,
-            signature
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            OWNER_PK,
+            keccak256(bytes(TEST_CID))
         );
 
-        vm.prank(user1);
-        nft.mint(user1, newCNFT);
-
+        nft.mint(
+            USER1,
+            BlockChefCampaignNFT.cNFT(
+                BlockChefCampaignNFT.cNFTtype.Gold,
+                TEST_CID,
+                abi.encodePacked(r, s, v) // This MUST be a valid signature
+            )
+        );
         assertEq(nft.totalSupply(), 1);
-        assertEq(nft.ownerOf(0), user1);
+        assertEq(nft.ownerOf(0), USER1);
         assertEq(
             nft.tokenURI(0),
-            string(abi.encodePacked(nft.IPFS_URL(), cid))
+            string(abi.encodePacked(nft.IPFS_URL(), TEST_CID))
         );
-        (BlockChefCampaignNFT.cNFTtype t,,) = nft.idToCNFT(0);
+        (BlockChefCampaignNFT.cNFTtype t, , ) = nft.idToCNFT(0);
         assertEq(uint8(t), uint8(BlockChefCampaignNFT.cNFTtype.Gold));
+        // expectEmit
+    }
+
+    function test_MintingWithFakeSignature() external {
+        nft.mint(
+            USER1,
+            BlockChefCampaignNFT.cNFT(
+                BlockChefCampaignNFT.cNFTtype.Gold,
+                TEST_CID,
+                hex"bd3351bc23afd50352db04db78f989b2d348b89a8a36a25278bd1bb34dea504f62fae7d264884152699d93cf7f27d63650a5258cc88cfe79996bd4037a2844021c" // This MUST be a fake signature"
+            )
+        );
+        assertEq(
+            nft.tokenURI(0),
+            string(abi.encodePacked(nft.IPFS_URL(), "null"))
+        );
+        (BlockChefCampaignNFT.cNFTtype t, , ) = nft.idToCNFT(0);
+        assertEq(uint8(t), uint8(BlockChefCampaignNFT.cNFTtype.Bronze));
     }
 
     function test_MintingFailsWithInvalidSignature() external {
-        // Preparing an invalid signature and cNFT data
-        bytes memory signature = hex"0011223344"; // This should be an invalid signature
-        string memory cid = "QmTestCID";
-        BlockChefCampaignNFT.cNFT memory invalidCNFT = BlockChefCampaignNFT
-            .cNFT(BlockChefCampaignNFT.cNFTtype.Gold, cid, signature);
-
-        vm.prank(user1);
         vm.expectRevert(
             abi.encodeWithSelector(
                 ECDSA.ECDSAInvalidSignatureLength.selector,
                 5
             )
         );
-        nft.mint(user1, invalidCNFT);
+        nft.mint(
+            USER1,
+            BlockChefCampaignNFT.cNFT(
+                BlockChefCampaignNFT.cNFTtype.Gold,
+                TEST_CID,
+                hex"1122334455" // This MUST be an invalid signature
+            )
+        );
     }
 
     function test_MintingWithAlreadyUsedCID() external {
-        uint256 ownerPK;
-        (, ownerPK) = owner();
-        string memory cid = "QmTestCID";
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPK, keccak256(bytes(cid)));
-        // This should be a valid signature
-        bytes memory signature = abi.encodePacked(r, s, v); // note the order here is different from line above.
-
-        BlockChefCampaignNFT.cNFT memory newCNFT = BlockChefCampaignNFT.cNFT(
-            BlockChefCampaignNFT.cNFTtype.Gold,
-            cid,
-            signature
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            OWNER_PK,
+            keccak256(bytes(TEST_CID))
         );
 
-        vm.prank(user1);
-        nft.mint(user1, newCNFT);
+        BlockChefCampaignNFT.cNFT memory validCNFT = BlockChefCampaignNFT.cNFT(
+            BlockChefCampaignNFT.cNFTtype.Diamond,
+            TEST_CID,
+            abi.encodePacked(r, s, v) // This MUST be a valid signature
+        );
 
-        // Attempt to mint again with the same CID
-        nft.mint(user2, newCNFT);
-
+        nft.mint(USER1, validCNFT);
+        nft.mint(USER2, validCNFT);
         assertEq(nft.totalSupply(), 2);
-        assertEq(nft.ownerOf(1), user2);
+        assertEq(nft.ownerOf(1), USER2);
         assertEq(
             nft.tokenURI(1),
             string(abi.encodePacked(nft.IPFS_URL(), "null"))
         );
-        (BlockChefCampaignNFT.cNFTtype t,,) = nft.idToCNFT(1);
+        (BlockChefCampaignNFT.cNFTtype t, , ) = nft.idToCNFT(1);
         assertEq(uint8(t), uint8(BlockChefCampaignNFT.cNFTtype.Bronze));
     }
 
     function test_MintingFallbacksToBronze() external {
-        // Invalid CID or signature
-        BlockChefCampaignNFT.cNFT memory invalidCNFT = BlockChefCampaignNFT
-            .cNFT(BlockChefCampaignNFT.cNFTtype.Gold, "InvalidCID", "");
-
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
-        nft.mint(user1, invalidCNFT);
+        (bool ok, ) = address(nft).call("");
+        assertEq(ok, true);
 
         (BlockChefCampaignNFT.cNFTtype t, string memory cid, ) = nft.idToCNFT(
             0
         );
         assertEq(uint8(t), uint8(BlockChefCampaignNFT.cNFTtype.Bronze));
         assertEq(cid, "null");
+        assertEq(nft.totalSupply(), 1);
     }
 
     function test_MintingNotAllowedWhenMintingLocked() external {
-        address _owner;
-        (_owner, ) = owner();
-
-        vm.prank(_owner);
+        vm.prank(OWNER);
         nft.oneTimeMintLocker();
 
         vm.expectRevert("NOT_MINTABLE_ANYMORE");
         nft.mint(
-            user1,
+            USER1,
             BlockChefCampaignNFT.cNFT(
-                BlockChefCampaignNFT.cNFTtype.Gold,
-                "CID",
-                "sig"
+                BlockChefCampaignNFT.cNFTtype.Bronze,
+                "",
+                ""
             )
         );
     }

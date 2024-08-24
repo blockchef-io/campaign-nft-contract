a17730e909e7a73c50f531128ba05b61aa32f1ea
FarajiOranj <farajioranj@gmail.com>
2024-08-24 17:32:25 +0330

    contract updated & improved

--- src/BlockChefCampaignNFT.sol
+++ src/BlockChefCampaignNFT.sol
@@ -28,7 +28,6 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
     |-*-*-*-*-*   STATES   *-*-*-*-*-|
     \********************************/
     bool public MINTABLE;
-    string public IPFS_URL;
     address public FORGE_DELEGATE_IMPLEMENTATION;
     uint256 public totalSupply;
     mapping(bytes32 => bool) public storedCID;
@@ -42,9 +41,21 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
     mapping(uint256 => cNFT) public idToCNFT;
     mapping(address => mapping(cNFTtype => uint256)) public ownerToTotalTypes;
 
-    uint256 public immutable BONUS_FEE;
-    uint256 public immutable SPECIALS_FEE;
+    address private immutable SIGN_PB_GATEWAY;
 
+    uint256 private constant BONUS_FEE = 0.00011 ether;
+    uint256 private constant SPECIALS_FEE = 0.00017 ether;
+    string private constant IPFS_URL = "https://ipfs.io/ipfs/";
+    string[] private CONSTANT_PATHS = [
+        //BONUS VOXEL
+        "https://ipfs.io/ipfs/QmbPkGZmFtip1PRZiNTTV8Darpg7atLtXN6Y1koWQ4WDiQ",
+        //SILVER VOXEL
+        "https://ipfs.io/ipfs/QmWMrB1hXxSDWVggh34tCPEuYf31jgdWLmfDuazF4TpR2X",
+        //GOLD VOXEL
+        "https://ipfs.io/ipfs/QmVuag2U9aGpk8DKZEC8iDxBy8tFhD9DHfBMFX5hfhyddo",
+        //DIAMOND VOXEL
+        "https://ipfs.io/ipfs/Qmdwtjf42L7M6uzzLaDcPYHwPSC8p6hZ6d3p7Sf6Hdc568"
+    ];
     /********************************\
     |-*-*-*-*-*   EVENTS   *-*-*-*-*-|
     \********************************/
@@ -62,19 +73,18 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
     );
 
     /********************************\
-    |-*-*-*-*-*  BUILT-IN  *-*-*-*-*-|
+    |-*-*-*-*    BUILT-IN    *-*-*-*-|
     \********************************/
-    constructor(
-        uint256 sfee,
-        uint256 bfee,
-        string memory ipfs_url
-    ) Ownable(msg.sender) ERC721("BlockChef Campaign NFT", "BC-CNFT") {
-        require(bytes(ipfs_url).length != 0, "CHECK_IPFS_URL");
-        require(sfee != 0 && bfee != 0, "ZERO_FEE_PROVIDED");
+    constructor(address signerPublicKeyAsGateway)
+        Ownable(msg.sender)
+        ERC721("BlockChef Campaign NFT", "BC-CNFT")
+    {
+        require(
+            signerPublicKeyAsGateway != address(0),
+            "ZERO_ADDRESS_PROVIDED"
+        );
 
-        SPECIALS_FEE = sfee;
-        BONUS_FEE = bfee;
-        IPFS_URL = ipfs_url;
+        SIGN_PB_GATEWAY = signerPublicKeyAsGateway;
         MINTABLE = true;
     }
 
@@ -107,6 +117,51 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
     /********************************\
     |-*-*-*    ERC721-LOGIC    *-*-*-|
     \********************************/
+    function mintSpecial(address to, cNFT memory nft) external payable {
+        require(MINTABLE, "NOT_MINTABLE_ANYMORE");
+        require(msg.value < SPECIALS_FEE, "PAY_FEE");
+
+        if (to == address(0)) to = msg.sender;
+        uint256 validValue = SPECIALS_FEE;
+
+        if (
+            balanceOf(to) != 0 ||
+            nft.t == cNFTtype.Bonus ||
+            bytes(nft.cid).length == 0 ||
+            nft.sign.length == 0 ||
+            storedCID[keccak256(abi.encodePacked(nft.cid))] ||
+            keccak256(abi.encodePacked(nft.cid, to)).recover(nft.sign) !=
+            SIGN_PB_GATEWAY
+        ) {
+            validValue = BONUS_FEE;
+            nft = cNFT({t: cNFTtype.Bonus, cid: "", sign: ""});
+        }
+
+        totalSupply++;
+        ownerToTotalTypes[to][nft.t]++;
+        typeToTotal[nft.t]++;
+        idToCNFT[totalSupply] = nft;
+        _mint(to, totalSupply);
+
+        if (nft.t == cNFTtype.Bonus) ownerBonusCNFTs[to].push(totalSupply);
+        else {
+            // Only 1 Special per EOA will be signed in Off-chain mechanisms
+            storedCID[keccak256(abi.encodePacked(nft.cid))] = true;
+            ownerCurrentSI[to] = 1;
+            ownerSpecialsDualMap[to][0] = totalSupply;
+            ownerSpecialsDualMap[to][totalSupply] = type(uint256).max;
+        }
+
+        if (msg.value > validValue) {
+            (bool sent, ) = payable(msg.sender).call{
+                value: (msg.value - validValue)
+            }("");
+            require(sent);
+        }
+
+        emit SpecialMint(to, totalSupply, nft.cid, nft.t);
+    }
+
     function batchBonusMint(address to) public payable {
         require(msg.value >= BONUS_FEE, "CHECK_BONUS_FEE");
         require(MINTABLE, "NOT_MINTABLE_ANYMORE");
@@ -146,47 +201,6 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
         }
     }
 
-    function mintSpecial(address to, cNFT memory nft) external payable {
-        require(MINTABLE, "NOT_MINTABLE_ANYMORE");
-        require(msg.value < SPECIALS_FEE, "PAY_FEE");
-        require(ownerToTotalCNFTs(to) == 0, "ONE_TIME_EVENT");
-
-        if (to == address(0)) to = msg.sender;
-
-        if (
-            nft.t == cNFTtype.Bonus ||
-            bytes(nft.cid).length == 0 ||
-            nft.sign.length == 0 ||
-            storedCID[keccak256(abi.encodePacked(nft.cid))] ||
-            keccak256(abi.encodePacked(nft.cid, to)).recover(nft.sign) !=
-            owner()
-        ) nft = cNFT({t: cNFTtype.Bonus, cid: "", sign: ""});
-
-        totalSupply++;
-        ownerToTotalTypes[to][nft.t]++;
-        typeToTotal[nft.t]++;
-        idToCNFT[totalSupply] = nft;
-        _mint(to, totalSupply);
-
-        if (nft.t == cNFTtype.Bonus) ownerBonusCNFTs[to].push(totalSupply);
-        else {
-            // Only 1 Special per EOA will be signed in Off-chain mechanisms
-            storedCID[keccak256(bytes(nft.cid))] = true;
-            ownerCurrentSI[to] = 1;
-            ownerSpecialsDualMap[to][0] = totalSupply;
-            ownerSpecialsDualMap[to][totalSupply] = type(uint256).max;
-        }
-
-        if (msg.value > SPECIALS_FEE) {
-            (bool sent, ) = payable(msg.sender).call{
-                value: (msg.value - SPECIALS_FEE)
-            }("");
-            require(sent);
-        }
-
-        emit SpecialMint(to, totalSupply, nft.cid, nft.t);
-    }
-
     function transferFrom(
         address from,
         address to,
@@ -209,6 +223,9 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
         ownerSpecialsDualMap[to][id] = type(uint256).max - ownerCurrentSI[to];
     }
 
+    /********************************\
+    |-*-*   ERC721-FORGE-LOGIC   *-*-|
+    \********************************/
     function forgeBonuses() external payable {
         require(
             FORGE_DELEGATE_IMPLEMENTATION != address(0),
@@ -219,20 +236,22 @@ contract BlockChefCampaignNFT is Ownable, ERC721 {
         require(ok, "CALL_FAILED");
     }
 
-    function tokenURI(uint256 id) public view override returns (string memory) {
-        return string(abi.encodePacked(IPFS_URL, idToCNFT[id].cid));
-    }
-
-    function ownerToTotalCNFTs(address account) public view returns (uint256) {
-        return (ownerToTotalTypes[account][cNFTtype.Diamond] +
-            ownerToTotalTypes[account][cNFTtype.Gold] +
-            ownerToTotalTypes[account][cNFTtype.Silver] +
-            ownerToTotalTypes[account][cNFTtype.Bonus]);
-    }
-
+    /********************************\
+    |-*-*-*-*-*    VIEW    *-*-*-*-*-|
+    \********************************/
     function hasSpecialCNFT(address account) external view returns (bool) {
         return (ownerToTotalTypes[account][cNFTtype.Diamond] != 0 ||
             ownerToTotalTypes[account][cNFTtype.Gold] != 0 ||
             ownerToTotalTypes[account][cNFTtype.Silver] != 0);
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        if (ownerOf(id) == address(0)) return "";
+
+        if (
+            keccak256(abi.encodePacked(idToCNFT[id].cid)) !=
+            keccak256(abi.encodePacked(""))
+        ) return string(abi.encodePacked(IPFS_URL, idToCNFT[id].cid));
+        else return CONSTANT_PATHS[uint8(idToCNFT[id].t)];
     }
 }
